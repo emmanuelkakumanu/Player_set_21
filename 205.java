8e109d9b3254f6e28d430cf7bee7e04288863cf1
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-11-22 19:01:52 +0530

    swap case

---
+++ 205.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		String str=s.next();
+		String re=new String();
+		for(int i=0;i<str.length();i++)
+		{
+			char ch=str.charAt(i);
+			if(Character.isUpperCase(ch))
+			{
+				re=re+Character.toLowerCase(ch);
+			}
+			else if(Character.isLowerCase(ch))
+			{
+				re=re+Character.toUpperCase(ch);
+			}
+			else
+			{
+				re=re+ch;
+			}
+		}
+		System.out.print(re);
+	}
+}
